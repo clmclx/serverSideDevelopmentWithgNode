be8b2acb8633d56eb55faaa8fe117b434e168504
clmclx <ccaylux@gmail.com>
2020-10-01 21:57:51 +0200

    initial commit

---
+++ nodejs/node-examples/index.js
@@ -0,0 +1,19 @@
+const rect =  {
+    perimeter: (x,y) => (2 * (x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("solving for rectangle with l = " + l + " and b = " +b);
+    if ( l<=0 || b <=0) {
+        console.log("error");
+    } else {
+        console.log("perimeter: " + rect.perimeter(l,b));
+        console.log("area: " + rect.area(l,b));
+    }
+};
+
+solveRect(3,4);
+solveRect(3,5);
+solveRect(0,4);
+solveRect( -3,4);
