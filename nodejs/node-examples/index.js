7612bb3414588c2645f27c0e3ca7cc0c4a45739a
clmclx <ccaylux@gmail.com>
2020-10-04 20:52:14 +0200

    Adding http example

--- nodejs/node-examples/index.js
+++ nodejs/node-examples/index.js
@@ -2,13 +2,16 @@ const rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("solving for rectangle with l = " + l + " and b = " +b);
-    if ( l<=0 || b <=0) {
-        console.log("error");
-    } else {
-        console.log("perimeter: " + rect.perimeter(l,b));
-        console.log("area: " + rect.area(l,b));
-    }
-};
+    rect(l,b, (err, rectangle) => {
+        if (err) {
+            console.log("error: " + err.message);
+        } else {
+            console.log("perimeter: " + rectangle.perimeter(l,b));
+            console.log("area: " + rectangle.area(l,b));
+        }
+    });
+    console.log("Called after");
+}
 
 solveRect(3,4);
 solveRect(3,5);
