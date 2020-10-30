a50122c5063d305cc38956178f3b435f4e5da046
clmclx <ccaylux@gmail.com>
2020-10-30 22:39:27 +0100

    start of promotion router

--- nodejs/conFusionServer/routes/promotionRouter.js
+++ nodejs/conFusionServer/routes/promotionRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const promotionRouter = express.Router();
+const mongoose = require('mongoose');
+
+const Promotions = require('../models/promotions');
 
 
 promotionRouter.use(bodyParser.json());
@@ -11,14 +14,26 @@ promotionRouter.route('/').all((req, res, next) => {
     res.setHeader('Content-Type', 'text/plain');
     next();
 }).get((req,res,next) => {
-    res.end('Will send all the promotions to you!');
+   Promotions.find({}).then(promotions => {
+       res.statusCode = 200;
+       res.setHeader('Content-Type', 'application/json');
+       res.json(promotions)
+   }, err => next(err)).catch(err => next(err));
 }).post((req, res, next) => {
-    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+    Promotions.create(req.body).then((promotion) =>{
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.json(promotion)
+    }, err => next(err)).catch(err => next(err))
 }).put((req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 }).delete((req, res, next) => {
-    res.end('Deleting all promotions');
+    Promotions.remove({}).then(resp => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.json(resp)
+    }, err => next(err)).catch(err => next(err))
 });
 
 // add all the endpoints for the /:promotionId endpoint
