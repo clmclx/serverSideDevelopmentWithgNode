c42cf6ebf539998e7702c43eca81611abf797311
clmclx <ccaylux@gmail.com>
2020-10-30 19:46:43 +0100

    full rest api part 1

--- nodejs/conFusionServer/app.js
+++ nodejs/conFusionServer/app.js
@@ -6,6 +6,18 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dishesRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promotionRouter');
+var leaderRouter = require('./routes/leaderRouter');
+
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then(db => {
+  console.log('Correctly connected to the db');
+});
 
 var app = express();
 
@@ -21,6 +33,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/dishes', dishesRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
